81e3524272f7f3fef895ea366c58b375de49a9a8
Anzhelika Heigl <anzhelika.heigl@edu.fh-joanneum.at>
2022-04-13 22:24:18 +0200

    exercise 3

---
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -0,0 +1,22 @@
+package at.fhj.msd;
+
+import java.util.Locale;
+
+public class Calculator {
+
+    public static double add(double number1, double number2) {
+        return number1 + number2;
+    }
+
+    public static double minus(double number1, double number2) {
+        return number1 - number2;
+    }
+
+    public static double multiply(double number1, double number2) {
+        return number1 * number2;
+    }
+
+    public static double divide(double number1, double number2) {
+        return number1 / number2;
+    }
+}
--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,2 +1,27 @@
-package at.fhj.msd;public class Main {
+package at.fhj.msd;
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("en", "US"));
+
+        //enter 2 double values
+        double value1 = 3.5;
+        double value2 = 7.7;
+
+
+        //compute the result
+        String print = "%.2f%s%.2f = %.2f\n";
+
+        System.out.printf(print, value1, "+", value2, Calculator.add(value1, value2));
+        System.out.printf(print, value1, "-", value2, Calculator.minus(value1, value2));
+        System.out.printf(print, value1, "*", value2, Calculator.multiply(value1, value2));
+        if (value2 == 0) {
+            System.out.println("Division " + value1 + "/" + value2 + " is not possible!");
+        } else {
+            System.out.printf(print, value1, "/", value2, Calculator.divide(value1, value2));
+        }
+    }
 }
+
